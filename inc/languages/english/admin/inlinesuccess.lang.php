c003cd7d7d0e6009286f4166fee5263a9dbfbc9e
Filippo <legend_k@live.it>
2013-08-27 23:54:56 +0200

    Version 1.0 mostly ready
    - Dramatically scaled down core edits to the essentials;
    - Core edits now affect and patch two core functions: redirect() and
    inline_error(). An inline success message is generated everytime the
    redirect() function is called, setting the global $errors variable as
    filled in with the parsed message from inline_success() function. This
    has 3 main positive aspects: it saves template edits, it saves a lot of
    work (it's a sort of "global" solution instead of a "local" one), and
    it allows the data to be available instantly instead of old data
    populating the inputs when no errors are encountered (basically, it
    tricks MyBB into checking for errors, but instead there's a success
    message).
    - The inline_error() patch ensures no errors are displayed at the same
    time of a success message.
    - The redirect() patch checks for "do_" substring in
    $mybb->input['action'] field and removes it to display the success
    message with the new data filled in as explained above.
    - A master switch setting has been added to let admins enable and
    disable the plugin effects with ease.
    - This new version is poorly tested but works fine as for the first
    tests. Feel free to install and check it out!

---
+++ inc/languages/english/admin/inlinesuccess.lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$l['inlinesuccess'] = "Inline Success Messages";
+$l['inlinesuccess_settings'] = "Inline Success Messages settings";
+$l['inlinesuccess_settings_desc'] = "Enable or disable Inline Success Messages without uninstalling it from your board.";
+$l['inlinesuccess_settings_enable'] = "Master Switch";
+$l['inlinesuccess_settings_enable_desc'] = "The master switch controls the plugin: if set to off, the plugin will be \"disabled\", it won't affect your board, but will be installed and ready to work just by enabling this option.";
--- inc/plugins/inlinesuccess.php
+++ inc/plugins/inlinesuccess.php
@@ -7,7 +7,7 @@
  * @package Inline Success Messages
  * @author  Shade <legend_k@live.it>
  * @license http://www.gnu.org/licenses/ GNU/GPL license
- * @version beta 2
+ * @version 1.0
  */
 
 if (!defined('IN_MYBB')) {
@@ -22,11 +22,11 @@ function inlinesuccess_info()
 {
 	return array(
 		'name' => 'Inline Success Messages',
-		'description' => 'Adds support for inline success messages in UCP instead of an (un)friendly redirection page.',
+		'description' => 'Adds support for inline success messages globally instead of an (un)friendly redirection page.',
 		'website' => 'http://www.idevicelab.net/forum',
 		'author' => 'Shade',
 		'authorsite' => 'http://www.idevicelab.net/forum',
-		'version' => 'beta 2',
+		'version' => '1.0',
 		'compatibility' => '16*',
 		'guid' => 'f6f2925d440239e6f3a894703ba088c6'
 	);
@@ -45,11 +45,15 @@ function inlinesuccess_is_installed()
 
 function inlinesuccess_install()
 {
-	global $db, $mybb, $cache, $PL;
+	global $db, $mybb, $cache, $PL, $lang;
 	
 	if (!file_exists(PLUGINLIBRARY)) {
 		flash_message("The selected plugin could not be installed because <a href=\"http://mods.mybb.com/view/pluginlibrary\">PluginLibrary</a> is missing.", "error");
 		admin_redirect("index.php?module=config-plugins");
+	}
+		
+	if (!$lang->inlinesuccess) {
+		$lang->load('inlinesuccess');
 	}
 	
 	$info = inlinesuccess_info();
@@ -62,156 +66,36 @@ function inlinesuccess_install()
 	
 	$PL or require_once PLUGINLIBRARY;
 	
-	// begin our huge core replacements!
-	$PL->edit_core('inlinesuccess', 'usercp.php', array(
-		// avatar
+	$PL->edit_core('inlinesuccess', 'inc/functions.php', array(
+		// redirect() function patch
 		array(
-			'search' => 'redirect("usercp.php", $lang->redirect_avatarupdated);',
-			'replace' => '$inlinesuccess = inline_success($lang->redirect_avatarupdated);
-$mybb->input[\'action\'] = "avatar";
-unset($avatar_error);'
-		),
-		// avatar - show new content directly
-		array(
-			'search' => 'eval("\$currentavatar = \"".$templates->get("usercp_avatar_current")."\";");',
-			'before' => 'if($updated_avatar) {
-	$urltoavatar = htmlspecialchars_uni($updated_avatar[\'avatar\']);
+			'search' => '$plugins->run_hooks("redirect", $redirect_args);',
+			'after' => 'if($mybb->settings[\'inlinesuccess_enabled\']) {
+	global $errors, $avatar_error;
+	$action = $mybb->input[\'action\'];
+	$find = "do_";
+	if(strpos($action, $find) !== FALSE) $mybb->input[\'action\'] = str_replace($find, "", $action);
+	$errors = $avatar_error = array("success" => 1,
+		"message" => $message
+	);
+	$errors = inline_success($errors);
+	return $errors;
 }'
 		),
-		// profile
+		// inline_error() function patch
 		array(
-			'search' => 'redirect("usercp.php", $lang->redirect_profileupdated);',
-			'replace' => '$inlinesuccess = inline_success($lang->redirect_profileupdated);
-$mybb->input[\'action\'] = "profile";
-unset($errors);'
-		),
-		// profile - show new content directly
-		array(
-			'search' => 'if($errors)
-	{
-		$user = $mybb->input;
-		$bday = array();
-		$bday[0] = $mybb->input[\'bday1\'];
-		$bday[1] = $mybb->input[\'bday2\'];
-		$bday[2] = intval($mybb->input[\'bday3\']);
+			'search' => 'foreach($errors as $error)',
+			'before' => 'if($mybb->settings[\'inlinesuccess_enabled\']) {
+	global $check;
+	if(!empty($errors[\'success\'])) {
+		if(empty($check)) {
+			$errors = inline_success($errors);
+		}
+		return $errors;
 	}
-	else
-	{
-		$user = $mybb->user;
-		$bday = explode("-", $user[\'birthday\']);
-	}',
-			'replace' => 'if($errors || $inlinesuccess)
-	{
-		$user = $mybb->input;
-		$bday = array();
-		$bday[0] = $mybb->input[\'bday1\'];
-		$bday[1] = $mybb->input[\'bday2\'];
-		$bday[2] = intval($mybb->input[\'bday3\']);
-	}
-	else
-	{
-		$user = $mybb->user;
-		$bday = explode("-", $user[\'birthday\']);
-	}'
+}'
 		),
-		// options
-		array(
-			'search' => 'redirect("usercp.php", $lang->redirect_optionsupdated);',
-			'replace' => '$inlinesuccess = inline_success($lang->redirect_optionsupdated);
-$mybb->input[\'action\'] = "options";
-unset($errors);'
-		),
-		// options - show new content directly
-		array(
-			'search' => 'if($errors != \'\')
-	{
-		$user = $mybb->input;
-	}
-	else
-	{
-		$user = $mybb->user;
-	}',
-			'replace' => 'if($errors || $inlinesuccess)
-	{
-		$user = $mybb->input;
-	}
-	else
-	{
-		$user = $mybb->user;
-	}'
-		),
-		// email
-		array(
-			'search' => 'redirect("usercp.php", $lang->redirect_emailupdated);',
-			'replace' => '$inlinesuccess = inline_success($lang->redirect_emailupdated);
-$mybb->input[\'action\'] = "email";
-unset($errors);'
-		),
-		// password
-		array(
-			'search' => 'redirect("usercp.php", $lang->redirect_passwordupdated);',
-			'replace' => '$inlinesuccess = inline_success($lang->redirect_passwordupdated);
-$mybb->input[\'action\'] = "password";
-unset($errors);'
-		),
-		// username
-		array(
-			'search' => 'redirect("usercp.php", $lang->redirect_namechanged);',
-			'replace' => '$inlinesuccess = inline_success($lang->redirect_namechanged);
-$mybb->input[\'action\'] = "changename";
-unset($errors);'
-		),
-		// signature
-		array(
-			'search' => 'redirect("usercp.php?action=editsig", $lang->redirect_sigupdated);',
-			'replace' => '$inlinesuccess = inline_success($lang->redirect_sigupdated);
-$mybb->input[\'action\'] = "editsig";
-unset($error);'
-		),
-		// notepad
-		array(
-			'search' => 'redirect("usercp.php", $lang->redirect_notepadupdated);',
-			'replace' => '$inlinesuccess = inline_success($lang->redirect_notepadupdated);
-$mybb->input[\'action\'] = \'\';'
-		)
 	), true);
-	
-	// ProfilePic plugin support - yay!
-	if (file_exists('inc/plugins/profilepic.php')) {
-		$PL->edit_core('inlinesuccess', 'inc/plugins/profilepic.php', array(
-			// profilepic plugin
-			array(
-				'search' => 'redirect("usercp.php", $lang->redirect_profilepicupdated);',
-				'replace' => '$inlinesuccess = inline_success($lang->redirect_profilepicupdated);
-$mybb->input[\'action\'] = "profilepic";
-unset($profilepic_error);'
-			)
-		), true);
-	}
-	
-	// Thought about XThreads (profile fields) too, although most of the people probably doesn't have it installed
-	if (file_exists('inc/plugins/xt_proffields.php')) {
-		
-		$PL->edit_core('inlinesuccess', 'inc/plugins/xt_proffields.php', array(
-			array(
-				'search' => 'global $templates,$mybb,$customfields,$lang,$theme,$user,$errors,$xtpf_inp,$xtpf_data;',
-				'replace' => 'global $templates,$mybb,$customfields,$lang,$theme,$user,$errors,$xtpf_inp,$xtpf_data,$inlinesuccess;'
-			),
-			array(
-				'search' => '$code = xt_proffields_inp($profilefield,$user,$errors,$vars);',
-				'replace' => '$code = xt_proffields_inp($profilefield,$user,$errors,$vars,$inlinesuccess);'
-			),
-			array(
-				'search' => 'function xt_proffields_inp(&$pa,&$user,&$errors,&$vars=array()){',
-				'replace' => 'function xt_proffields_inp(&$pa,&$user,&$errors,&$vars=array(),&$inlinesuccess){'
-			),
-			array(
-				'search' => 'if($errors){',
-				'replace' => 'if($errors || $inlinesuccess){'
-			)
-		), true);
-		
-	}
 	
 	// Euantor's templating system	   
 	$dir = new DirectoryIterator(dirname(__FILE__) . '/InlineSuccess/templates');
@@ -224,9 +108,13 @@ unset($profilepic_error);'
 	
 	$PL->templates('inlinesuccess', 'Inline Success Messages', $templates);
 	
-	find_replace_multitemplatesets('{$errors}', '{$errors}{$inlinesuccess}');
-	find_replace_multitemplatesets('{$avatar_error}', '{$avatar_error}{$inlinesuccess}');
-	find_replace_multitemplatesets('{$profilepic_error}', '{$profilepic_error}{$inlinesuccess}');
+	$PL->settings('inlinesuccess', $lang->inlinesuccess_settings, $lang->inlinesuccess_settings_desc, array(
+		'enabled' => array(
+			'title' => $lang->inlinesuccess_settings_enable,
+			'description' => $lang->inlinesuccess_settings_enable_desc,
+			'value' => '1'
+		),
+	));
 	
 }
 
@@ -247,20 +135,11 @@ function inlinesuccess_uninstall()
 	unset($shadePlugins[$info['name']]);
 	$cache->update('shade_plugins', $shadePlugins);
 	
-	// oh dear, so many core edits passing away!
-	$PL->edit_core('inlinesuccess', 'usercp.php', array(), true);
+	// oh dear, core edits are passing away!
+	$PL->edit_core('inlinesuccess', 'inc/functions.php', array(), true);
 	
-	if (file_exists('inc/plugins/profilepic.php')) {
-		$PL->edit_core('inlinesuccess', 'inc/plugins/profilepic.php', array(), true);
-	}
-	
-	if (file_exists('inc/plugins/xt_proffields.php')) {
-		$PL->edit_core('inlinesuccess', 'inc/plugins/xt_proffields.php', array(), true);
-	}
-	
-	$PL->templates_delete('inlinesuccess');
-	
-	find_replace_multitemplatesets('{$inlinesuccess}', '');
+	$PL->templates_delete('inlinesuccess');	
+	$PL->settings_delete('inlinesuccess');
 }
 
 /**
@@ -272,7 +151,7 @@ function inlinesuccess_uninstall()
 
 function inline_success($messages)
 {
-	global $theme, $mybb, $db, $lang, $templates;
+	global $theme, $mybb, $db, $lang, $templates, $check, $errors;
 	
 	if (!is_array($messages)) {
 		$messages = array(
@@ -280,47 +159,14 @@ function inline_success($messages)
 		);
 	}
 	
+	// populating this global variable to ensure no inline success messages inside error messages
+	$check = 1;
+	
 	foreach ($messages as $message) {
 		$messagelist .= $message . "\n";
 	}
 	
-	eval("\$inlinesuccess = \"" . $templates->get("inlinesuccess_success_inline") . "\";");
+	eval("\$errors = \"" . $templates->get("inlinesuccess_success_inline") . "\";");
 	
-	return $inlinesuccess;
-}
-
-/**
- * Simple find and replace function for multiple templates at once.
- *
- * @param string The pattern to find in templates, thus will be replaced by the replacement.
- * @param string The string that will replace the pattern.
- * @return boolean True if successful, false if unsuccessful
- */
-
-function find_replace_multitemplatesets($find, $replace)
-{
-	global $db, $mybb;
-	
-	$return = false;
-	
-	// Select all other modified templates with that title
-	$query = $db->simple_select("templates", "tid, sid, template, title", "template LIKE '%" . $db->escape_string($find) . "%'");
-	while ($template = $db->fetch_array($query)) {
-		// replace the content
-		$new_template = str_replace($find, $replace, $template['template']);
-		if ($new_template == $template['template']) {
-			continue;
-		}
-		
-		// The template is a custom template. Replace as normal.
-		$updated_template = array(
-			"template" => $db->escape_string($new_template)
-		);
-		
-		$db->update_query("templates", $updated_template, "tid='{$template['tid']}'");
-		
-		$return = true;
-	}
-	
-	return $return;
+	return $errors;
 }
